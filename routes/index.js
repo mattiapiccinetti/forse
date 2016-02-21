a0e41a2fa28b1441b8ef436434241a6f98aec57a
Mattia Piccinetti <mattia.piccinetti@gmail.com>
2016-02-21 19:40:25 +0100

    indentation madness

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 router.get('/', function (req, res) {
   res.render('layouts/index', {
+      title: 'forse',
       apiEndpoint: req.protocol + '://' + req.headers.host + '/api/tips/random',
       defaultTip: 'forse non funziona'
     });
