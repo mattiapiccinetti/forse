cada55d7d5fff1ffcea4dbb55bd3e12656ab7c46
Mattia Piccinetti <mattia.piccinetti@jobrapido.com>
2016-11-12 14:13:12 +0100

    random tip could be called with a post #justForTest

--- routes/tips.js
+++ routes/tips.js
@@ -7,23 +7,55 @@ var superMegaSecretTip = process.env.SUPER_MEGA_SECRET_TIP;
 
 router.get('/tips/random', (req, res) => {
   pg.connect(conString, (err, client, done) => {
-    if (err) { return console.error('error fetching client from pool', err); }
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
 
     client.query('select * from tips', (err, result) => {
       done();
-      if (err) { return console.error(err); }
+      if (err) {
+        return console.error(err);
+      }
       res.send(result.rows[Math.floor(Math.random() * result.rows.length)])
     });
   });
 });
 
+router.post('/tips/random', (req, res) => {
+
+  pg.connect(conString, (err, client, done) => {
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+
+    client.query('select * from tips', (err, result) => {
+      done();
+      if (err) {
+        return console.error(err);
+      }
+      res.send({
+        "color": "green",
+        "message": result.rows[Math.floor(Math.random() * result.rows.length)],
+        "notify": false,
+        "message_format": "text"
+      })
+    });
+  });
+});
+
+
+
 router.get('/tips/:id(\\d+)', (req, res) => {
   pg.connect(conString, (err, client, done) => {
-    if (err) { return console.error('error fetching client from pool', err); }
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
 
     client.query('select * from tips where id = $1', [req.params.id], (err, result) => {
       done();
-      if (err) { return console.error(err); }
+      if (err) {
+        return console.error(err);
+      }
       res.send(result.rows[0]);
     });
   });
@@ -31,15 +63,21 @@ router.get('/tips/:id(\\d+)', (req, res) => {
 
 router.get('/tips', (req, res) => {
   if (req.query.secret !== superMegaSecretTip) {
-    return res.status(401).json({ message: 'forse non sei autorizzato' });
+    return res.status(401).json({
+      message: 'forse non sei autorizzato'
+    });
   }
 
   pg.connect(conString, (err, client, done) => {
-    if (err) { return console.error('error fetching client from pool', err); }
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
 
     client.query('select * from tips order by id asc', (err, result) => {
       done();
-      if (err) { return console.error(err); }
+      if (err) {
+        return console.error(err);
+      }
       res.send(result.rows);
     });
   });
@@ -47,32 +85,48 @@ router.get('/tips', (req, res) => {
 
 router.post('/tips', (req, res) => {
   if (req.query.secret !== superMegaSecretTip) {
-    return res.status(401).json({ message: 'forse non sei autorizzato' });
+    return res.status(401).json({
+      message: 'forse non sei autorizzato'
+    });
   }
 
   pg.connect(conString, (err, client, done) => {
-    if (err) { return console.error('error fetching client from pool', err); }
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
 
     client.query('insert into tips(tip) values($1)', [req.body.tip], (err, result) => {
       done();
-      if (err) { return console.error(err); }
-      res.status(201).json({ message: 'tip inserito forse con successo' });
+      if (err) {
+        return console.error(err);
+      }
+      res.status(201).json({
+        message: 'tip inserito forse con successo'
+      });
     });
   });
 });
 
 router.put('/tips/:id(\\d+)', (req, res) => {
   if (req.query.secret !== superMegaSecretTip) {
-    return res.status(401).json({ message: 'forse non sei autorizzato' });
+    return res.status(401).json({
+      message: 'forse non sei autorizzato'
+    });
   }
 
   pg.connect(conString, (err, client, done) => {
-    if (err) { return console.error('error fetching client from pool', err); }
-    
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+
     client.query('update tips set tip = $1, created_at = now() where id = $2', [req.body.tip, req.params.id], (err, result) => {
       done();
-      if (err) { return console.error(err); }
-      res.json({ message: 'tip aggiornato forse con successo' });
+      if (err) {
+        return console.error(err);
+      }
+      res.json({
+        message: 'tip aggiornato forse con successo'
+      });
     });
   });
 });
